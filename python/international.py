2ef45e629da625aac6bb4e36ceb083fbdcbe39e9
sksum <55449862+sksum@users.noreply.github.com>
2020-09-13 17:42:25 +0530

    international

--- python/international.py
+++ python/international.py
@@ -5,6 +5,8 @@ import datetime
 def parseTime(time):
     return time
 
+from bbc import bbc
+
 headers = {
     "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0",
@@ -17,38 +19,13 @@ listN = soup.find_all(class_ = "nw-c-top-stories__secondary-item")
 
 topNews = []
 for site in listN :
-    heading = site.find('a').h3.text
-    heading = heading.replace('\xa0', ' ').encode('utf-8')
     link = baseURL + site.find('a')["href"]
-    time = site.find('time')["datetime"]
-    time = parseTime(time)
-    news = {
-        "href": link,
-        "headline": heading,
-        "time": time
-    }
-    topNews.append(news)
+    topNews.append(bbc(link))
 # print(topNews)
 
 listN = soup.find_all(class_ = "nw-c-top-stories__tertiary-top")
 listN.extend(soup.find_all(class_ = "nw-c-top-stories__tertiary-items"))
 
-# print(listN)
 for site in listN:
-    # print(site)
-    heading = site.find('a').h3.text
-    heading = heading.replace('\xa0', ' ').encode('utf-8')
-    print(heading)
     link = baseURL + site.find('a')["href"]
-    try:
-        time = site.find('time')["datetime"]
-        time = parseTime(time)
-    except:
-        time = datetime.datetime.now()
-    news = {
-        "href": link,
-        "headline": heading,
-        "time": time
-    }
-    topNews.append(news)
-print(topNews)
+    topNews.append(bbc(link))
