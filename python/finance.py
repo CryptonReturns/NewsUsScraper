a0fd41378764a360dd54e6aa3d0c7fbbf71065ce
sksum <55449862+sksum@users.noreply.github.com>
2020-09-13 17:32:27 +0530

    finance + general

--- python/finance.py
+++ python/finance.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup 
 import requests 
 from summarize import getSummary
-
+from financialexpress import financialExpress
 def parseTime(time):
     return time
 
@@ -19,36 +19,31 @@ listN = soup.find_all(class_ = "listitembx")
 topNews = []
 for site in listN :
     heading = site.find('h3').a.text
-    heading = heading.replace('\xa0', ' ').encode('utf-8')
+    # heading = heading.replace('\xa0', ' ').encode('utf-8')
     link = site.find('h3').a["href"]
-    time = site.find(class_ = "minsago").text
-    time = parseTime(time)
-    news = {
-        "href": link,
-        "headline": heading,
-        "time": time
-    }
-    topNews.append(news)
+    # time = site.find(class_ = "minsago").text
+    # time = parseTime(time)
+    topNews.append(financialExpress(link))
 
-listSite = "https://economictimes.indiatimes.com/news/economy"
-mainSite = "https://economictimes.indiatimes.com"
-page = requests.get(listSite, headers=headers)
-soup = BeautifulSoup(page.content, 'html.parser')
-listN = soup.find(class_ = "list1").find_all('li') 
+# listSite = "https://economictimes.indiatimes.com/news/economy"
+# mainSite = "https://economictimes.indiatimes.com"
+# page = requests.get(listSite, headers=headers)
+# soup = BeautifulSoup(page.content, 'html.parser')
+# listN = soup.find(class_ = "list1").find_all('li') 
 
-topNews2 = []
-for site in listN :
-    heading = site.find('a').text
-    heading = heading.replace('\xa0', ' ').encode('utf-8')
-    link = mainSite + site.find('a')["href"]
-    time = site.find('time')['data-time']
-    time = parseTime(time)
-    news = {
-        "href": link,
-        "headline": heading,
-        "time": time
-    }
-    topNews2.append(news)
+# topNews2 = []
+# for site in listN :
+#     heading = site.find('a').text
+#     heading = heading.replace('\xa0', ' ').encode('utf-8')
+#     link = mainSite + site.find('a')["href"]
+#     time = site.find('time')['data-time']
+#     time = parseTime(time)
+#     news = {
+#         "href": link,
+#         "headline": heading,
+#         "time": time
+#     }
+#     topNews2.append(news)
 
 
-print(topNews2)
+# print(topNews2)
--- python/general.py
+++ python/general.py
@@ -4,7 +4,7 @@ from summarize import getSummary
 import datetime
 def parseTime(time):
     return time
-
+from toi import toi
 headers = {
     "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     "User-Agent" : "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:80.0) Gecko/20100101 Firefox/80.0",
@@ -17,18 +17,15 @@ listN = soup.find(class_ = "list9").find_all('li')
 
 topNews = []
 for site in listN :
-    print(site)
     try:
         heading = site.find('a').text
         heading = heading.replace('\xa0', ' ').encode('utf-8')
         link = baseURL + site.a["href"]
-        news = {
-            "href": link,
-            "headline": heading,
-            # "time": time
-        }
-        topNews.append(news)
+        # news = {
+        #     "href": link,
+        #     "headline": heading,
+        #     # "time": time
+        # }
+        topNews.append(toi(link))
     except:
         print("Non list item ")
-
-print(topNews)
